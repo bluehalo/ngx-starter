65c6a729871c0215b9ed165f93e76243b73cd9e4
John Rassa <jrassa@asymmetrik.com>
2019-03-19 10:01:20 -0400

    scss cleanup.  replaced bootstrap style declarations with variable overrides where appropriate

--- src/app/common/system-alert/system-alert.component.ts
+++ src/app/common/system-alert/system-alert.component.ts
@@ -4,7 +4,8 @@ import { SystemAlertService } from './system-alert.service';
 
 @Component({
 	selector: 'system-alert',
-	templateUrl: 'system-alert.component.html'
+	templateUrl: 'system-alert.component.html',
+	styleUrls: [ 'system-alert.component.scss' ]
 })
 export class SystemAlertComponent {
 	constructor(public alertService: SystemAlertService) {}
