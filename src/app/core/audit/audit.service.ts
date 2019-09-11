c2632f8ee6182074a2bb07f0b8388725733ec2cb
Brian Ghigiarelli <brianghig@gmail.com>
2019-09-10 21:26:10 -0400

    Allowing external components to register actions for View Details and View Changes modals

--- src/app/core/audit/audit.service.ts
+++ src/app/core/audit/audit.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import isArray from 'lodash/isArray';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PagingOptions, PagingResults } from '../../common/paging.module';
 import _isArray from 'lodash/isArray';
+import _includes from 'lodash/includes';
 
 import { User } from '../auth/user.model';
 
 @Injectable()
 export class AuditService {
 	constructor(private http: HttpClient) {}
+
+	private viewDetailsActions: string[] = [
+		'create',
+		'delete'
+	];
+	private viewChangesActions: string[] = [
+		'update',
+		'admin update'
+	];
+
+	public isViewDetailsAction(action: string) {
+		return _includes(this.viewDetailsActions, action)
+	}
+
+	public addViewDetailsAction(action: string) {
+		this.viewDetailsActions.push(action);
+	}
+
+	public isViewChangesAction(action: string) {
+		return _includes(this.viewChangesActions, action)
+	}
+
+	public addViewChangesAction(action: string) {
+		this.viewChangesActions.push(action);
+	}
 
 	public getDistinctAuditValues(field: string): Observable<string[]> {
 		return this.http.get<string[]>('api/audit/distinctValues', { params: { field: field } });
@@ -31,7 +57,7 @@ export class AuditService {
 			{ params: paging.toObj() }
 		).pipe(
 			map((results: PagingResults) => {
-				if (null != results && isArray(results.elements)) {
+				if (null != results && _isArray(results.elements)) {
 					results.elements = results.elements.map((element: any) => new User().setFromUserModel(element));
 				}
 				return results;
