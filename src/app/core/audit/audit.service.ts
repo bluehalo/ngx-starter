beee6341415ce56df0ac3c1eaf9d321581513a94
Brian Ghigiarelli <brianghig@gmail.com>
2019-09-11 19:50:29 -0400

    Exporting classes from the Audit Module (#76)
    * Exporting classes from the Audit Module in order to allow external components, like the site, to register their own audit visualization components
    
    * Allowing external components to register actions for View Details and View Changes modals
    
    * Fixing lint issues
    
    * Updating to use native array includes method.

--- src/app/core/audit/audit.service.ts
+++ src/app/core/audit/audit.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import isArray from 'lodash/isArray';
+
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PagingOptions, PagingResults } from '../../common/paging.module';
@@ -10,7 +10,34 @@ import { User } from '../auth/user.model';
 
 @Injectable()
 export class AuditService {
+
+	private viewDetailsActions: string[] = [
+		'create',
+		'delete'
+	];
+
+	private viewChangesActions: string[] = [
+		'update',
+		'admin update'
+	];
+
 	constructor(private http: HttpClient) {}
+
+	public isViewDetailsAction(action: string) {
+		return this.viewDetailsActions.includes(action);
+	}
+
+	public addViewDetailsAction(action: string) {
+		this.viewDetailsActions.push(action);
+	}
+
+	public isViewChangesAction(action: string) {
+		return this.viewChangesActions.includes(action);
+	}
+
+	public addViewChangesAction(action: string) {
+		this.viewChangesActions.push(action);
+	}
 
 	public getDistinctAuditValues(field: string): Observable<string[]> {
 		return this.http.get<string[]>('api/audit/distinctValues', { params: { field: field } });
@@ -31,7 +58,7 @@ export class AuditService {
 			{ params: paging.toObj() }
 		).pipe(
 			map((results: PagingResults) => {
-				if (null != results && isArray(results.elements)) {
+				if (null != results && _isArray(results.elements)) {
 					results.elements = results.elements.map((element: any) => new User().setFromUserModel(element));
 				}
 				return results;
