b9ee39c99eb6d6c81365931215caf0fb80536f00
John Rassa <jrassa@asymmetrik.com>
2019-11-13 15:58:54 -0500

    using async pipe to avoid manual subscription handling

--- src/app/common/system-alert/system-alert.component.ts
+++ src/app/common/system-alert/system-alert.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { SystemAlertService } from './system-alert.service';
-import { SystemAlert } from './system-alert.model';
-import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'system-alert',
 	templateUrl: 'system-alert.component.html',
 	styleUrls: [ 'system-alert.component.scss' ]
 })
-export class SystemAlertComponent implements OnInit, OnDestroy {
-	constructor(public alertService: SystemAlertService) { }
-	private sub: Subscription;
-	alerts: SystemAlert[];
-
-	ngOnInit() {
-		this.sub = this.alertService.alerts$.subscribe((alerts) => {
-			this.alerts = alerts;
-		});
-	}
-
-	ngOnDestroy() {
-		this.sub.unsubscribe();
-	}
+export class SystemAlertComponent {
+	constructor(public alertService: SystemAlertService) {}
 }
