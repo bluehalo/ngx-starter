db3a3165ad113c28ea88278d5b36dab7de6c5bd8
Sumeet Shah <sshah@asymmetrik.com>
2020-03-20 13:04:48 -0400

    feat: update admin components to use tableAction templates

--- src/app/common/paging/pageable-table/pageable-table.component.ts
+++ src/app/common/paging/pageable-table/pageable-table.component.ts
@@ -14,8 +14,9 @@ export class PageableTableComponent {
 	@Input() loading = false;
 	@Input() showInCard = false;
 	@Input() showActions = false;
-	@Input() hideActionsNoData = true;
+	@Input() hideActionsNoData = false;
 	@Input() showFooterActions = false;
+	@Input() hideFooterActionsNoData = false;
 	@Input() hideTableNoData = false;
 	@Input() disableGoToEnd = false;
 	@Input() pagerAtTop = false;
