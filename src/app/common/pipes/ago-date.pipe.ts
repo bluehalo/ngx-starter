4313c7a8ffe57eb335b410b2ae87cdf92cfab1f6
John Rassa <jrassa@asymmetrik.com>
2020-07-07 13:38:04 -0400

    Misc fixes (#138)
    * fix: Sorting of admin eua list
    
    Undefined default sort resulted in error that prevented further changes to sorting.  Fixed by setting default sort.
    
    * fix: Disable sorting of 'Submitted By' column in admin feedback list
    
    'Submitted By' columns sorting config was invalid due to leftover remenant of when feedback was stored in audit logs.  Removed config and set sortable to false, since stored value is just an id.
    
    * fix: Update AgoDatePipe to accept string input
    
    Implementation already handles strings properly.  Just need to update types on method call.
    
    * fix: Remove cursor pointer style from search icon on SearchInputComponent
    
    Icon is decorative, there is no click effect, so no need for pointer cursor
    
    * refactor: Remove outdated bootstrap class
    
    'form-horizontal' class is no longer part of bootstrap and is no longer necessary.

--- src/app/common/pipes/ago-date.pipe.ts
+++ src/app/common/pipes/ago-date.pipe.ts
@@ -4,7 +4,7 @@ import * as moment from 'moment';
 
 @Pipe({ name: 'agoDate' })
 export class AgoDatePipe implements PipeTransform {
-	transform(date: Date | number, hideAgo?: boolean): string {
+	transform(date: Date | string | number, hideAgo?: boolean): string {
 		if (null != date) {
 			// If it's not null, and it's either a number or a date
 			return moment(date).fromNow(hideAgo);
