889d254bd9a4f57e94c49ca4525edd8547f86ed7
Brian Ghigiarelli <brianghig@gmail.com>
2020-08-26 09:07:37 -0400

    #92 Fixing list-audit-entries component spec by waiting for results before expecting. Cleaned up various lint warnings

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -39,7 +39,7 @@ export class AuthGuard implements CanActivate {
 			first(),
 			catchError(error => {
 				// Handle the error
-				console.log(error);
+				console.error(error);
 				return of(null);
 			})
 		);
