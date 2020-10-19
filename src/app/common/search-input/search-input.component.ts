c2f28f2e3a6bec7dd1dabdff9a84dac7867d976f
nrwinner <nwinner@asymmetrik.com>
2020-10-19 11:37:28 -0400

    :art: Refactor SearchInputComponent to accept a min character count and optionally use the input event instead of the keyup event

--- src/app/common/search-input/search-input.component.ts
+++ src/app/common/search-input/search-input.component.ts
@@ -1,24 +1,69 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@UntilDestroy()
 @Component({
 	selector: 'asy-search-input',
 	templateUrl: './search-input.component.html',
-	styleUrls: ['./search-input.component.scss']
+	styleUrls: ['./search-input.component.scss'],
+	animations: [
+		trigger('minCount', [
+			transition(':enter', [
+				style({ opacity: 0, height: 0 }),
+				animate('200ms ease', style({ opacity: 1, height: '*' }))
+			]),
+			transition(':leave', [
+				style({ opacity: 1, height: '*' }),
+				animate('200ms ease', style({ opacity: 0, height: 0 }))
+			])
+		])
+	]
 })
 export class SearchInputComponent {
 	@Input() placeholder = 'Search...';
 	@Output() readonly applySearch: EventEmitter<string> = new EventEmitter();
 	@Input() search = '';
+	@Input() width = '350px';
 
-	private keyupTimeout: any;
+	/**
+	 * If true, searches will be made on `input` events, otherwise searches will be made on `keyup` events
+	 */
+	@Input() preferInputEvent: boolean = false;
 
-	constructor() {}
+	/**
+	 * Specifies a minimum character count required to search.
+	 * In the event the number of characters is between 0 and the minimum, a warning message is shown beneath the search bar
+	 */
+	@Input() minSearchCharacterCount: number = 0;
+
+	searchInput$ = new Subject<void>();
+	showMinCountMessage = false;
+
+	constructor() {
+		this.searchInput$.pipe(debounceTime(350), untilDestroyed(this)).subscribe(() => {
+			if (this.search.length === 0 || this.search.length >= this.minSearchCharacterCount) {
+				this.showMinCountMessage = false;
+				this.applySearch.emit(this.search);
+			} else {
+				this.showMinCountMessage = true;
+			}
+		});
+	}
 
 	onKeyup() {
-		clearTimeout(this.keyupTimeout);
-		this.keyupTimeout = setTimeout(() => {
-			this.applySearch.emit(this.search);
-		}, 350);
+		if (!this.preferInputEvent) {
+			this.searchInput$.next();
+		}
+	}
+
+	onInput() {
+		if (this.preferInputEvent) {
+			this.searchInput$.next();
+		}
 	}
 
 	clearSearch(event?: MouseEvent) {
