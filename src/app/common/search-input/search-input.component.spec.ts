a93941513c0b5097da9275542b34558434edccfd
nrwinner <nwinner@asymmetrik.com>
2020-10-20 13:00:43 -0400

    :white_check_mark: Add new test suite for SearchInputComponent

---
+++ src/app/common/search-input/search-input.component.spec.ts
@@ -0,0 +1,156 @@
+import { ElementRef, EventEmitter } from '@angular/core';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SearchInputComponent } from './search-input.component';
+
+describe('SearchInputComponent', () => {
+	let fixture: ComponentFixture<SearchInputComponent>,
+		componentInstance: SearchInputComponent,
+		inputElement: ElementRef<HTMLInputElement>;
+
+	beforeEach(() => {
+		fixture = TestBed.configureTestingModule({
+			declarations: [SearchInputComponent],
+			imports: [FormsModule, NoopAnimationsModule]
+		}).createComponent(SearchInputComponent);
+
+		componentInstance = fixture.componentInstance;
+
+		// replace the emit function of applySearch with a spy
+		componentInstance.applySearch.emit = jasmine.createSpy();
+		inputElement = fixture.debugElement.query(By.css('input'));
+
+		fixture.detectChanges();
+	});
+
+	it('should apply search on `keyup` event by default', fakeAsync(() => {
+		inputElement.nativeElement.value = 'search value';
+		fixture.detectChanges();
+		// need to trigger input event here so ngModel will set `search` property correctly
+		// this line by itself will not trigger a search
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+
+		// this line should trigger a search
+		inputElement.nativeElement.dispatchEvent(new Event('keyup'));
+		tick(350);
+
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledTimes(1);
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledWith('search value');
+	}));
+
+	it('should not apply the search if keyup is never triggered', fakeAsync(() => {
+		inputElement.nativeElement.value = 'search value';
+		fixture.detectChanges();
+		// need to trigger input event here so ngModel will set `search` property correctly
+		// this line by itself will not trigger a search
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+
+		tick(350);
+
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledTimes(0);
+	}));
+
+	it('should apply search if preferInputEvent is true and an input event is trigered', fakeAsync(() => {
+		componentInstance.preferInputEvent = true;
+		inputElement.nativeElement.value = 'search value';
+		fixture.detectChanges();
+
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+
+		tick(350);
+
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledTimes(1);
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledWith('search value');
+	}));
+
+	it('should not apply search if minSearchCharacterCount is specified not adhered to', fakeAsync(() => {
+		componentInstance.minSearchCharacterCount = 3;
+		inputElement.nativeElement.value = 'se';
+		fixture.detectChanges();
+
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+		inputElement.nativeElement.dispatchEvent(new Event('keyup'));
+
+		tick(350);
+
+		expect(componentInstance.applySearch.emit).not.toHaveBeenCalled();
+	}));
+
+	it('should show warning message if minSearchCharacterCount is specified and not adhered to', fakeAsync(() => {
+		componentInstance.minSearchCharacterCount = 3;
+		inputElement.nativeElement.value = 'se';
+		fixture.detectChanges();
+
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+		inputElement.nativeElement.dispatchEvent(new Event('keyup'));
+
+		tick(350);
+
+		fixture.detectChanges();
+
+		expect(
+			(fixture.debugElement.query(By.css('.min-count')).nativeElement as HTMLElement)
+				.textContent
+		).toEqual('Searches require a minimum of 3 characters.');
+	}));
+
+	it('should not show warning message if minSearchCharacterCount is specified and adhered to', fakeAsync(() => {
+		componentInstance.minSearchCharacterCount = 3;
+		inputElement.nativeElement.value = 'sea';
+		fixture.detectChanges();
+
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+		inputElement.nativeElement.dispatchEvent(new Event('keyup'));
+
+		tick(350);
+
+		fixture.detectChanges();
+
+		expect(fixture.debugElement.query(By.css('.min-count'))).toBeNull();
+	}));
+
+	it('should show clear-search options if search input length > 0', () => {
+		inputElement.nativeElement.value = 'search';
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+		fixture.detectChanges();
+
+		expect(fixture.debugElement.queryAll(By.css('.icon.fa.fa-times')).length).toBe(1);
+	});
+
+	it('should not show clear-search options if search input length === 0', () => {
+		inputElement.nativeElement.value = '';
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+		fixture.detectChanges();
+
+		expect(fixture.debugElement.queryAll(By.css('span.icon.fa-times')).length).toBe(0);
+	});
+
+	it('should clear the search when the clearSearch span is clicked', async(() => {
+		const clearSearchSpy = spyOn(componentInstance, 'clearSearch').and.callThrough();
+
+		inputElement.nativeElement.value = 'search';
+		inputElement.nativeElement.dispatchEvent(new Event('input'));
+		fixture.detectChanges();
+
+		// click the clear-search option
+		(fixture.debugElement.query(By.css('span.icon.fa-times'))
+			.nativeElement as HTMLElement).click();
+
+		fixture.detectChanges();
+
+		expect(clearSearchSpy).toHaveBeenCalledTimes(1);
+		expect(componentInstance.search).toBe('');
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledTimes(1);
+		expect(componentInstance.applySearch.emit).toHaveBeenCalledWith('');
+
+		fixture.whenRenderingDone().then(() => {
+			expect(
+				(fixture.debugElement.query(By.css('input')).nativeElement as HTMLInputElement)
+					.value
+			).toBe('');
+		});
+	}));
+});
