3972896414595d02f3657a04747069e83ecf3e9e
Brian Ghigiarelli <bghigiarelli@asymmetrik.com>
2021-04-05 09:56:36 -0400

    Adding component-based modal to example page

---
+++ src/app/site/example/modal/form-modal.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+import { AbstractModalizableDirective } from '../../../common/modal/abstract-modalizable.directive';
+
+@UntilDestroy()
+@Component({
+	templateUrl: 'form-modal.component.html'
+})
+export class FormModalComponent extends AbstractModalizableDirective {
+	constructor(public modalRef: BsModalRef) {
+		super();
+	}
+
+	onOk() {
+		this.modalRef.hide();
+	}
+	onCancel() {
+		this.modalRef.hide();
+	}
+}
