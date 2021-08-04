32ed3aa549cf7aec68dd31c9ae629fd726e22fce
Aedan Pettit <apettit@asymmetrik.com>
2021-08-04 10:35:23 -0400

    Updated ago-date pipe

--- src/app/common/pipes/ago-date.pipe.ts
+++ src/app/common/pipes/ago-date.pipe.ts
@@ -1,13 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import * as moment from 'moment';
+import { DateTime } from 'luxon';
 
 @Pipe({ name: 'agoDate' })
 export class AgoDatePipe implements PipeTransform {
 	transform(date: Date | string | number, hideAgo?: boolean): string {
 		if (null != date) {
+			let agoDate;
 			// If it's not null, and it's either a number or a date
-			return moment(date).fromNow(hideAgo);
+			if (typeof date === 'number') {
+				agoDate = DateTime.fromMillis(date).toRelative();
+			} else if (date instanceof Date) {
+				agoDate = DateTime.fromJSDate(date).toRelative();
+			}
+
+			if (hideAgo && agoDate.indexOf('ago')) {
+				return agoDate.replace(' ago', '');
+			} else {
+				return agoDate;
+			}
 		}
 
 		return 'unknown';
