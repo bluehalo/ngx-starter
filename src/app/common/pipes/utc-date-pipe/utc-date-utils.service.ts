ec5e1064030e3f8a23bf9bdd825aeab954fff32e
John Rassa <jrassa@asymmetrik.com>
2022-07-06 10:36:16 -0400

    fix: Update UtcDateUtils.format to support formatting JS Date objects

--- src/app/common/pipes/utc-date-pipe/utc-date-utils.service.ts
+++ src/app/common/pipes/utc-date-pipe/utc-date-utils.service.ts
@@ -3,11 +3,16 @@ import { DateTime } from 'luxon';
 export class UtcDateUtils {
 	private static defaultFormat = 'yyyy-MM-dd HH:mm:ss';
 
-	static format(value: string | number | DateTime | null | undefined, format?: string): string {
+	static format(
+		value: string | number | Date | DateTime | null | undefined,
+		format?: string
+	): string {
 		if (null != value) {
 			let luxonDate;
 			if (value instanceof DateTime) {
 				luxonDate = value;
+			} else if (value instanceof Date) {
+				luxonDate = DateTime.fromJSDate(value).toUTC();
 			} else if (typeof value === 'number') {
 				luxonDate = DateTime.fromMillis(value).toUTC();
 			} else if (typeof value === 'string') {
