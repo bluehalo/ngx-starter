7a4856e1ddabcb8f592ca772ac8a9e1901ccb331
clu@asymmetrik.com <clu@asymmetrik.com>
2023-02-07 11:49:43 -0500

    Updated comment description for this directive

--- src/app/common/directives/link-accessibility.directive.ts
+++ src/app/common/directives/link-accessibility.directive.ts
@@ -1,9 +1,10 @@
 import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 /**
- * Used to mark where in a page the "Skip to main content" link
- * should skip to. It is ideally placed on the page's main heading:
- * the h1 element. This directive should only be used once per page
+ * Used to enable link for keyboard accessibility.
+ * This is only necessary for interactibles using that do not have a
+ * href attribute, because those will naturally be ignored by
+ * the tabbing structure.
  */
 @Directive({
 	selector: '[linkAccessibility]'
