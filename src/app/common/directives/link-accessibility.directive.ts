30037939fc5118d0f9ea0c1de2f2e1651a3ea3d7
John Rassa <jrassa@asymmetrik.com>
2023-02-08 16:21:04 -0500

    feat: Improve accessibility of navbar (#304)
    * Updated navbar links so you can now navigate to them using keyboard tabbing. This happened because <a> elements with no href attribute will be ignored, and to a user with navigating the website through keyboard alone, those links will be completely inaccessible. This commit adds a directive that adds the `tabindex="0"`, and then forces an artificial click on submit to copy native link behavior
    
    
    Co-authored-by: clu@asymmetrik.com <clu@asymmetrik.com>

---
+++ src/app/common/directives/link-accessibility.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+/**
+ * Used to enable link for keyboard accessibility.
+ * This is only necessary for interactibles using that do not have a
+ * href attribute, because those will naturally be ignored by
+ * the tabbing structure.
+ */
+@Directive({
+	selector: '[linkAccessibility]'
+})
+export class LinkAccessibilityDirective {
+	constructor(private elRef: ElementRef, private renderer: Renderer2) {
+		const el = elRef.nativeElement;
+		renderer.setAttribute(el, 'tabIndex', '0');
+	}
+
+	@HostListener('keydown.enter', ['$event'])
+	onEnter(event: Event) {
+		let targetEl: any = event.target;
+		targetEl?.dispatchEvent(new Event('click'));
+	}
+}
