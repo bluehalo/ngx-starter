db012fa43df32c9afef45ca53d151d616f656113
John Rassa <john.rassa@bluehalo.com>
2023-05-12 10:50:11 -0400

    build: Update to angular 15
    * updates Angular to 15 and updates other dependencies to compatible versions.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-	imports: [RouterModule.forRoot([], { useHash: true, relativeLinkResolution: 'legacy' })],
+	imports: [RouterModule.forRoot([], { useHash: true })],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {}
