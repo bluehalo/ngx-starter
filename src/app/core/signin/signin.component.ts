cc9c30f6723254c8be433f8a1c64829e319ac997
John Rassa <john.rassa@bluehalo.com>
2024-05-08 08:03:19 -0400

    build: Migrate to new Angular application builder

--- src/app/core/signin/signin.component.ts
+++ src/app/core/signin/signin.component.ts
@@ -33,7 +33,7 @@ export class SigninComponent {
 	private navigationService = inject(NavigationService);
 	private config = inject(APP_CONFIG);
 
-	pkiMode = computed(() => this.config()?.auth === 'proxy-pki' ?? false);
+	pkiMode = computed(() => this.config()?.auth === 'proxy-pki');
 
 	constructor() {
 		if (this.pkiMode()) {
